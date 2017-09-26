1714d72b13b281db2917b5094dce954679a2aef3
Elijah Caluya <elicaluya@yahoo.com>
2017-09-25 22:41:55 -0700

    adding basic server

---
+++ PsychResearchProject/src/client.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.net.*;
+
+public class client {
+	static int PORT = 8800;
+	static String HOST = "localhost";
+	public static void main(String argv[]) throws Exception{
+		String sentence;
+		String modifiedSentence;
+		BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+		
+		Socket clientSocket = new Socket(HOST,PORT);
+		DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+		BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+		
+		sentence = inFromUser.readLine();
+		outToServer.writeBytes(sentence + '\n');
+		modifiedSentence = inFromServer.readLine();
+		System.out.println(modifiedSentence);
+		clientSocket.close();
+	}
+}
